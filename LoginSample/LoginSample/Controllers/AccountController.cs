916bed4ed214d79a6a42821913eaea47c93a010c
Fernando Apolinário <fernandoapolinario@outlook.com>
2017-09-09 20:36:17 -0300

    LoginPage

---
+++ LoginSample/LoginSample/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using LoginSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginSample.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            ViewBag.Test = "ahhh";
+            return View();
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            ModelState.AddModelError("Password", "hhhhh");
+            ModelState.AddModelError("Email", "llllll");
+            return View();
+        }
+    }
+}
--- LoginSample/LoginSample/Controllers/HomeController.cs
+++ LoginSample/LoginSample/Controllers/HomeController.cs
@@ -26,5 +26,6 @@ namespace LoginSample.Controllers
 
             return View();
         }
+        
     }
 }
